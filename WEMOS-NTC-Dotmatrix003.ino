d2fa57daa20095ece2aa351c010adb889a36415c
chingchai  kuendee <chingchai.th@gmail.com>
2018-05-12 15:59:33 +0700

    003
    003

---
+++ WEMOS-NTC-Dotmatrix003.ino
@@ -0,0 +1,345 @@
+/*
+   Wemos TimeNTP_ESP8266WiFi.ino
+   This sketch uses the ESP8266WiFi library
+*/
+
+#include <TimeLib.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Max72xxPanel.h>
+
+void webloop();
+
+int pinCS = D6;
+int numberOfHorizontalDisplays = 4;
+int numberOfVerticalDisplays = 1;
+char time_value[20];
+char time_value1[20];
+
+// LED Matrix Pin -> ESP8266 Pin
+// Vcc -> 3v  (3V on NodeMCU 3V3 on WEMOS)
+// Gnd -> Gnd (G on NodeMCU)
+// DIN -> D7  (Same Pin for WEMOS)
+// CS  -> D6  (Same Pin for WEMOS)
+// CLK -> D5  (Same Pin for WEMOS)
+
+Max72xxPanel matrix = Max72xxPanel(pinCS, numberOfHorizontalDisplays, numberOfVerticalDisplays);
+
+int wait = 150; // In milliseconds
+
+int spacer = 1;
+int width = 5 + spacer; // The font width is 5 pixels
+
+const char ssid[] = "GreatODev";  //  your network SSID (name)
+const char pass[] = "1683vios";       // your network password
+
+const char ssidAP[] = "ESP8266-NTP";  //  your network SSID (name)
+const char passAP[] = "";       // your network password
+
+// NTP Servers:
+static const char ntpServerName[] = "th.pool.ntp.org";
+//static const char ntpServerName[] = "time.nist.gov";
+//static const char ntpServerName[] = "time-a.timefreq.bldrdoc.gov";
+//static const char ntpServerName[] = "time-b.timefreq.bldrdoc.gov";
+//static const char ntpServerName[] = "time-c.timefreq.bldrdoc.gov";
+
+const int timeZone = 7;     // Thailand
+//const int timeZone = -5;  // Eastern Standard Time (USA)
+//const int timeZone = -4;  // Eastern Daylight Time (USA)
+//const int timeZone = -8;  // Pacific Standard Time (USA)
+//const int timeZone = -7;  // Pacific Daylight Time (USA)
+
+
+WiFiUDP Udp;
+unsigned int localPort = 8888;  // local port to listen for UDP packets
+
+time_t getNtpTime();
+void digitalClockDisplay();
+void printDigits(int digits);
+void sendNTPpacket(IPAddress &address);
+
+//-----AP Mode-------------------------
+IPAddress    apIP(42, 42, 42, 42);     // Defining a static IP address: local & gateway
+
+// Define a web server at port 80 for HTTP
+ESP8266WebServer server(80);
+const int ledPin = D4; // an LED is connected to NodeMCU pin D4 (ESP8266 GPIO2) via a 1K Ohm resistor
+const int ledPin1 = D1; // an LED is connected to NodeMCU pin D4 (ESP8266 GPIO2) via a 1K Ohm resistor
+
+bool ledState = false;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial) ; // Needed for Leonardo only
+  delay(250);
+  Serial.println("TimeNTP Example");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, pass);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+
+  Serial.println("Starting UDP");
+  Udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(Udp.localPort());
+  Serial.println("waiting for sync");
+  setSyncProvider(getNtpTime);
+  Serial.print("IP number assigned by DHCP is ");
+  Serial.println(WiFi.localIP());
+  setSyncInterval(500);
+
+
+  matrix.setIntensity(0); // Use a value between 0 and 15 for brightness
+  matrix.setRotation(0, 1); // The first display is position upside down
+  matrix.setRotation(1, 1); // The first display is position upside down
+  matrix.setRotation(2, 1); // The first display is position upside down
+  matrix.setRotation(3, 1); // The first display is position upside down
+
+  pinMode ( ledPin, OUTPUT );
+  digitalWrite (ledPin, 0 );
+  digitalWrite (ledPin1, 0 );
+
+  Serial.println();
+  Serial.println("Configuring access point...");
+
+  //set-up the custom IP address
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));   // subnet FF FF FF 00  
+  
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssidAP, passAP);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+ 
+  server.on ( "/", handleRoot );
+  server.on ( "/led=1", handleRoot);
+  server.on ( "/led=0", handleRoot);
+  server.on ( "/inline", []() {
+    server.send ( 200, "text/plain", "this works as well" );
+  } );
+  server.onNotFound ( handleNotFound );
+  
+  server.begin();
+  Serial.println("HTTP server started");
+  
+}
+
+time_t prevDisplay = 0; // when the digital clock was displayed
+
+void loop()
+{
+
+  if (timeStatus() != timeNotSet) {
+    if (now() != prevDisplay) {                              //update the display only if time has changed
+      prevDisplay = now();
+      digitalClockDisplay();
+
+      //char zero = '0';
+
+      matrix.fillScreen(LOW);
+
+      String tempHour;
+      String tempMinute;
+      if (hour() < 10) {
+        tempHour = "0" + hour();
+      } else {
+        tempHour = hour();
+      }
+
+      if (minute() < 10) {tempMinute = "0" + hour();
+      } else {
+        tempMinute = hour();
+      }
+      tempHour.toCharArray(time_value, 10);
+      tempMinute.toCharArray(time_value1, 10);
+      
+      matrix.drawChar(2, 0, time_value[0], HIGH, LOW, 1);    // H
+      matrix.drawChar(8, 0, time_value[1], HIGH, LOW, 1);    // HH
+      matrix.drawChar(14, 0, 58, HIGH, LOW, 1);
+      matrix.drawChar(20, 0, time_value1[0], HIGH, LOW, 1); // HH:M
+      matrix.drawChar(26, 0, time_value1[1], HIGH, LOW, 1); // HH:MM
+      matrix.write();                                       // Send bitmap to display
+
+      Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+
+      delay(500);
+      matrix.drawChar(14, 0, 0, HIGH, LOW, 1);
+      matrix.write();
+      
+    }
+  }
+  server.handleClient();
+}
+
+void digitalClockDisplay()
+{
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(".");
+  Serial.print(month());
+  Serial.print(".");
+  Serial.print(year());
+  Serial.println();
+}
+
+void printDigits(int digits)
+{
+  // utility for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+
+/*-------- NTP code ----------*/
+
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
+
+time_t getNtpTime()
+{
+  IPAddress ntpServerIP; // NTP server's ip address
+
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP Request");
+  // get a random server from the pool
+  WiFi.hostByName(ntpServerName, ntpServerIP);
+  Serial.print(ntpServerName);
+  Serial.print(": ");
+  Serial.println(ntpServerIP);
+  sendNTPpacket(ntpServerIP);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP Response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+  Serial.println("No NTP Response :-(");
+  return 0; // return 0 if unable to get the time
+}
+
+// send an NTP request to the time server at the given address
+void sendNTPpacket(IPAddress &address)
+{
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12] = 49;
+  packetBuffer[13] = 0x4E;
+  packetBuffer[14] = 49;
+  packetBuffer[15] = 52;
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+//------------Server------------------
+
+void handleRoot() {
+  digitalWrite (LED_BUILTIN, 0); //turn the built in LED on pin DO of NodeMCU on
+  digitalWrite (ledPin, server.arg("led").toInt());
+  ledState = digitalRead(ledPin);
+
+ /* Dynamically generate the LED toggle link, based on its current state (on or off)*/
+  char ledText[80];
+  
+  if (ledState) {
+    strcpy(ledText, "LED is on. <a href=\"/?led=0\">Turn it OFF!</a>");
+  }
+
+  else {
+    strcpy(ledText, "LED is OFF. <a href=\"/?led=1\">Turn it ON!</a>");
+  }
+ 
+  ledState = digitalRead(ledPin);
+
+  char html[1000];
+  int sec = millis() / 1000;
+  int min = sec / 60;
+  int hr = min / 60;
+
+  int brightness = analogRead(A0);
+  brightness = (int)(brightness + 5) / 10; //converting the 0-1024 value to a (approximately) percentage value
+
+// Build an HTML page to display on the web-server root address
+  snprintf ( html, 1000,
+
+"<html>\
+  <head>\
+    <meta http-equiv='refresh' content='10'/>\
+    <title>ESP8266 WiFi Network</title>\
+    <style>\
+      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; font-size: 1.5em; Color: #000000; }\
+      h1 { Color: #AA0000; }\
+    </style>\
+  </head>\
+  <body>\
+    <h1>ESP8266 Wi-Fi Access Point and Web Server Demo</h1>\
+    <p>Uptime: %02d:%02d:%02d</p>\
+    <p>Brightness: %d%</p>\
+    <p>%s<p>\
+    <p>This page refreshes every 10 seconds. Click <a href=\"javascript:window.location.reload();\">here</a> to refresh the page now.</p>\
+  </body>\
+</html>",
+
+    hr, min % 60, sec % 60,
+    brightness,
+    ledText
+  );
+  server.send ( 200, "text/html", html );
+  digitalWrite ( LED_BUILTIN, 1 );
+}
+
+void handleNotFound() {
+  digitalWrite ( LED_BUILTIN, 0 );
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+
+  for ( uint8_t i = 0; i < server.args(); i++ ) {
+    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+  }
+
+  server.send ( 404, "text/plain", message );
+  digitalWrite ( LED_BUILTIN, 1 ); //turn the built in LED on pin DO of NodeMCU off
+}
+
+
